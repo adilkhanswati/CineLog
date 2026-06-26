acb121aa3e67c4e5a555dfcae06a0a4d0483e198
Adil Khan <kj3557983@gmail.com>
2026-06-26 09:48:19 +0500

    Add register and login view models

---
+++ Models/AuthViewModels.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CineLog.Models;
+
+public class RegisterViewModel
+{
+    [Required]
+    [StringLength(40, MinimumLength = 3)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string Password { get; set; } = string.Empty;
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
+
+public class LoginViewModel
+{
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
