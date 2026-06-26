66dc46216e27c8274eac2b997636a12d6dceef7f
Aman Khan <amankkhan1023@gmail.com>
2026-06-26 09:48:04 +0500

    Add AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using CineLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineLog.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
