b6d04721b4c6ca364a6c29a902cbdb732c456880
Adil Khan <kj3557983@gmail.com>
2026-06-26 09:47:48 +0500

    Add landing page and black-and-white styles

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using CineLog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineLog.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        if (User.Identity?.IsAuthenticated == true)
+            return RedirectToAction("Index", "Movies");
+
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
